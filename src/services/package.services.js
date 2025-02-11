03ebd1b8e8c620029af8b6b542575a79ed49bf68
Chí Bảo <tribao5556@gmail.com>
2025-02-11 15:42:13 +0700

    package services created

---
+++ src/services/package.services.js
@@ -0,0 +1,8 @@
+const PACKAGE = require("../models/package.model");
+const APIError = require("../utils/ApiError");
+class PackageServices {
+  async getAllPackage() {}
+  async updatePackage() {}
+  async deletePackage() {}
+}
+module.exports = PackageServices;

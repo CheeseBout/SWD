0765cff224aa41433ed5f063742db27310749fc7
Nhật Long <longcpnse183738@fpt.edu.vn>
2025-02-19 10:17:18 +0700

    rename the controller

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,9 +1,9 @@
-const authServices = require("../services/auth.services");
-const catchAsync = require("../utils/catchAsync");
-const { OK } = require("../utils/response");
+import authServices from "../services/auth.services.js";
+import catchAsync from "../utils/catchAsync.js";
+import { OK } from "../utils/response.js";
 
-const authController = {
-  register: catchAsync(async (req, res) => {
+class AuthController {
+  register = catchAsync(async (req, res) => {
     const { fullname, username, email, password, dob, gender, photoURL, role } =
       req.body;
 
@@ -21,9 +21,9 @@ const authController = {
         role,
       })
     );
-  }),
+  });
 
-  updateExpertProfile: catchAsync(async (req, res) => {
+  updateExpertProfile = catchAsync(async (req, res) => {
     const {
       title, // certificate title
       issuedDate, // certificate issue date
@@ -47,37 +47,38 @@ const authController = {
         category,
       })
     );
-  }),
+  });
 
-  login: catchAsync(async (req, res) => {
+  login = catchAsync(async (req, res) => {
     const { email, password } = req.body;
     return OK(res, "Success", await authServices.login({ email, password }));
-  }),
+  });
 
-  sendVerifyEmail: catchAsync(async (req, res) => {
+  sendVerifyEmail = catchAsync(async (req, res) => {
     const { email } = req.body;
     return OK(res, "Success", await authServices.sendVerifyEmail({ email }));
-  }),
+  });
 
-  verifyEmail: catchAsync(async (req, res) => {
+  verifyEmail = catchAsync(async (req, res) => {
     const { token } = req.query;
     const { email } = req.body;
     return OK(res, "Success", await authServices.verifyEmail({ email, token }));
-  }),
+  });
 
-  forgotPassword: catchAsync(async (req, res) => {
+  forgotPassword = catchAsync(async (req, res) => {
     const { email } = req.body;
     return OK(res, "Success", await authServices.forgotPassword({ email }));
-  }),
+  });
 
-  resetPassword: catchAsync(async (req, res) => {
+  resetPassword = catchAsync(async (req, res) => {
     const { resetToken, email, password } = req.body;
     return OK(
       res,
       "Success",
       await authServices.resetPassword({ resetToken, email, password })
     );
-  }),
+  })
 };
 
-module.exports = authController;
+const authController = new AuthController();
+export default authController;

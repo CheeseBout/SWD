ecd1557ac5d8dcb1254409aaa7b0c1ebd58475d5
CheeseBout <tribao5556@gmail.com>
2025-03-04 23:57:51 +0700

    auth added to rating

--- src/routes/rating.route.js
+++ src/routes/rating.route.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const ratingController = require("../controllers/rating.controller");
+const auth = require("../middlewares/auth.middleware");
 
-router.post("/create-rating", ratingController.createRating);
+router.post("/create-rating", auth, ratingController.createRating);
 router.get("/get-all-rating", ratingController.getAllRating);
 router.get("/get-rating/:ratingID", ratingController.getRatingById);
-router.put("/update-rating/:ratingID", ratingController.updateRating);
-router.put("/delete-rating/:ratingID", ratingController.deleteRating);
+router.put("/update-rating/:ratingID", auth, ratingController.updateRating);
+router.put("/delete-rating/:ratingID", auth, ratingController.deleteRating);
 
 module.exports = router;
